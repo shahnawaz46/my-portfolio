0df0e2dc59a95edd1758e4096ba28555d26c38c4
shahnawaz <shahnawazmd791@gmail.com>
2023-08-14 23:43:35 +0530

    fix smooth scroll and navbar underline

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,50 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { AiOutlineClose } from "react-icons/ai";
-import { BiMenuAltRight } from "react-icons/bi";
-import { useParams, useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { AiOutlineClose } from 'react-icons/ai';
+import { BiMenuAltRight } from 'react-icons/bi';
 
 const navItems = [
-  { name: "Home", link: "" },
-  { name: "About", link: "/#about" },
-  { name: "Skills", link: "/#skill" },
+  { name: 'Home', link: '', minUnderlinePos: 0, maxUnderlinePos: 500 },
+  {
+    name: 'About',
+    link: '#about',
+    minUnderlinePos: 501,
+    maxUnderlinePos: 1005,
+  },
+  {
+    name: 'Skills',
+    link: '#skill',
+    minUnderlinePos: 1006,
+    maxUnderlinePos: 1433,
+  },
   // { name: "Work", link: "/#work" },
-  { name: "Project", link: "/#project" },
+  {
+    name: 'Project',
+    link: '#project',
+    minUnderlinePos: 1434,
+    maxUnderlinePos: 3000,
+  },
 ];
 
 const Navbar = () => {
-  const pathname = usePathname();
-  const router = useRouter()
   const [showNavbar, setShowNavbar] = useState(false);
   const [navbarBackground, setNavbarBackground] = useState(false);
+  const [navbarUnderline, setNavbarUnderline] = useState(0);
 
   const onScroll = () => {
-    window.scrollY > 80
-      ? setNavbarBackground(true)
-      : setNavbarBackground(false);
+    if (window.scrollY > 80) setNavbarBackground(true);
+    else setNavbarBackground(false);
+
+    const { scrollY } = window;
+    console.log('scrollY', scrollY);
+    setNavbarUnderline(scrollY);
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
   // useEffect(() => {
@@ -40,12 +55,12 @@ const Navbar = () => {
   return (
     <div
       className={`sticky top-0 z-20 text-white ${
-        navbarBackground && "bg-black"
+        navbarBackground && 'bg-black bottom-8'
       } flex justify-between items-center h-16 px-3 sm:p-5 md:px-12 lg:px-24`}
     >
       <h1 className="text-3xl">
-        {" "}
-        &lt;Profo<span className="text-[#3CCF91]">\</span>&gt;{" "}
+        {' '}
+        &lt;Profo<span className="text-[#3CCF91]">\</span>&gt;{' '}
       </h1>
       <BiMenuAltRight
         className="text-3xl cursor-pointer sm:hidden"
@@ -53,7 +68,7 @@ const Navbar = () => {
       />
       <nav
         className={`fixed sm:static top-0 ${
-          showNavbar ? "right-0" : "right-[-100%]"
+          showNavbar ? 'right-0' : 'right-[-100%]'
         } transition-all bg-white sm:bg-transparent text-black sm:text-white w-60 sm:w-fit h-screen sm:h-fit p-4 flex flex-col sm:flex-row gap-3 sm:gap-7 lg:gap-12 cursor-pointer z-10`}
       >
         <div className="flex justify-end">
@@ -66,8 +81,9 @@ const Navbar = () => {
           <div
             key={index}
             className={`text-xl sm:text-lg ${
-              pathname === `${item.link}` &&
-              "underline underline-offset-4 decoration-[#3CCF91]"
+              navbarUnderline >= item.minUnderlinePos &&
+              navbarUnderline <= item.maxUnderlinePos &&
+              'underline underline-offset-4 decoration-[#3CCF91]'
             } sm:hover:underline sm:hover:decoration-[#3CCF91] sm:hover:underline-offset-4`}
           >
             <Link href={item.link}>{item.name}</Link>
